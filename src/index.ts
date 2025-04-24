5e5ff482bc3b782001daeca675e0bdacaa0b3990
Joelcio Façanha <joelciomoreira@gmail.com>
2025-04-24 10:12:28 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world dev");
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({ message: "Api funcionando!" });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
